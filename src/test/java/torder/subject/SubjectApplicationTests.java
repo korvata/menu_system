e2dcdfdc7c22fbb052e15936b0df9909cb0a1857
korvata <korvata@naver.com>
2021-07-18 17:01:02 +0900

    Initial commit

---
+++ src/test/java/torder/subject/SubjectApplicationTests.java
@@ -0,0 +1,13 @@
+package torder.subject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
