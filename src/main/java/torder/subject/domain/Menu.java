5b47d0ce8199c629db56f425c8958766938deb74
korvata <korvata@naver.com>
2021-07-18 17:02:21 +0900

    메뉴 엔티티

---
+++ src/main/java/torder/subject/domain/Menu.java
@@ -0,0 +1,18 @@
+package torder.subject.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter @Setter
+public class Menu {
+
+    @Id
+    private String id;      //메뉴ID
+
+    private String name;    //메뉴이름
+    private int price;      //메뉴가격
+}
