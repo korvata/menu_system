2123e0c1d58733914ed1cc72b67c9fdd1de1b80b
korvata <korvata@naver.com>
2021-07-19 01:38:37 +0900

    주문 기능

---
+++ src/main/java/torder/subject/controller/OrderController.java
@@ -0,0 +1,47 @@
+package torder.subject.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import torder.subject.domain.Cart;
+import torder.subject.domain.Member;
+import torder.subject.domain.Order;
+import torder.subject.repository.MemberRepository;
+import torder.subject.service.OrderService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+    private final MemberRepository memberRepository;
+
+    //주문
+    @PostMapping("/order")
+    @ResponseBody
+    public void order(@RequestParam(value = "menuArr[]") List<String> menuArr) {
+
+        String memberId = "yjh";//임시 사용자ID
+
+        for(String menu : menuArr){
+            log.info(menu);
+        }
+
+        List<Cart> carts = new ArrayList<>();
+
+        Long orderId = orderService.order(memberId, carts);
+
+        log.info("order success!");
+        log.info("orderId = {}", orderId);
+    }
+
+
+}
