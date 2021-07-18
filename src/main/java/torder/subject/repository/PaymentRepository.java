428657dc0657b3a4f42b158bc46f6a85f561f11c
korvata <korvata@naver.com>
2021-07-18 17:06:01 +0900

    결제 레퍼지토리

---
+++ src/main/java/torder/subject/repository/PaymentRepository.java
@@ -0,0 +1,39 @@
+package torder.subject.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import torder.subject.domain.Order;
+import torder.subject.domain.Payment;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class PaymentRepository {
+
+    private final EntityManager em;
+
+    public void save(Payment payment) {
+        if (payment.getId() == null) {
+            em.persist(payment);
+        } else {
+            em.merge(payment);
+        }
+    }
+
+    public Payment findOne(Long id) {
+        return em.find(Payment.class, id);
+    }
+
+    public List<Payment> findAll() {
+        return em.createQuery("select p from Payment p", Payment.class)
+                .getResultList();
+    }
+
+    public Payment findByOrder(Order order){
+        return findAll().stream()
+                .filter(p->p.getOrder().equals(order))
+                .findFirst().get();
+    }
+}
