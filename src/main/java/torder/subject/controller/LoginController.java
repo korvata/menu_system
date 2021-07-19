78bd6987b5c2925afcd5b2240dc52c2773972936
korvata <korvata@naver.com>
2021-07-19 21:40:56 +0900

    로그인 및 세션 기능

---
+++ src/main/java/torder/subject/controller/LoginController.java
@@ -0,0 +1,49 @@
+package torder.subject.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import torder.subject.domain.Member;
+import torder.subject.login.LoginForm;
+import torder.subject.service.LoginService;
+import torder.subject.session.SessionConst;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class LoginController {
+
+    private final LoginService loginService;
+
+    @PostMapping("/login")
+    public String login(@ModelAttribute LoginForm form, BindingResult bindingResult,
+                        @RequestParam(defaultValue = "/") String redirectURL,
+                        HttpServletRequest request) {
+
+        if (bindingResult.hasErrors()) {
+            return "login/loginForm";
+        }
+
+        Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+
+        if (loginMember == null) {
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
+            return "login/loginForm";
+        }
+
+
+        HttpSession session = request.getSession();
+
+        session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
+
+        return "redirect:" + redirectURL;
+
+    }
+}
---
+++ src/main/java/torder/subject/login/LoginForm.java
@@ -0,0 +1,10 @@
+package torder.subject.login;
+
+import lombok.Data;
+
+@Data
+public class LoginForm {
+
+    private String loginId;
+    private String password;
+}
