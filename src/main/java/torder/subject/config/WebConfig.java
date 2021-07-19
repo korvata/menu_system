5f04577bfd59b864332ae60ce237a6cf0b162036
korvata <korvata@naver.com>
2021-07-19 23:02:12 +0900

    웹 설정파일

---
+++ src/main/java/torder/subject/config/WebConfig.java
@@ -0,0 +1,24 @@
+package torder.subject.config;
+
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import torder.subject.login.LoginCheckInterceptor;
+import torder.subject.login.LoginMemberArgumentResolver;
+
+import java.util.List;
+
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginMemberArgumentResolver());
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/login", "/logout");
+    }
+}
