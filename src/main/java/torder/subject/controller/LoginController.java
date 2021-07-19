30ddd5c16cd0a86b1d8e39f65be39dda869ca5dd
korvata <korvata@naver.com>
2021-07-19 22:18:10 +0900

    로그인 창 이동

--- src/main/java/torder/subject/controller/LoginController.java
+++ src/main/java/torder/subject/controller/LoginController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,6 +22,11 @@ import javax.servlet.http.HttpSession;
 public class LoginController {
 
     private final LoginService loginService;
+
+    @GetMapping("/login")
+    public String loginForm(@ModelAttribute("loginForm") LoginForm form) {
+        return "login/loginForm";
+    }
 
     @PostMapping("/login")
     public String login(@ModelAttribute LoginForm form, BindingResult bindingResult,
