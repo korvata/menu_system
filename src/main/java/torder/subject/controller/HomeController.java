ae5aaebfa8427f13e86ffb14979499b74cadce3f
korvata <korvata@naver.com>
2021-07-20 01:09:15 +0900

    로깅 추가 : 세션에 저장된 ID

--- src/main/java/torder/subject/controller/HomeController.java
+++ src/main/java/torder/subject/controller/HomeController.java
@@ -1,22 +1,26 @@
 package torder.subject.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import torder.subject.domain.Member;
 import torder.subject.session.SessionConst;
 
+@Slf4j
 public class HomeController {
+
 
     @GetMapping("/")
     public String index(
-            @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember,
-            Model model) {
+            @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember
+            , Model model) {
 
         if (loginMember == null) {
             return "index";
         }
 
+        log.info(loginMember.getId());
         model.addAttribute("member", loginMember);
         return "menus/menuList";
     }
--- src/main/java/torder/subject/controller/MenuController.java
+++ src/main/java/torder/subject/controller/MenuController.java
@@ -5,8 +5,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import torder.subject.domain.Member;
 import torder.subject.domain.Menu;
 import torder.subject.service.MenuService;
+import torder.subject.session.SessionConst;
 
 import java.util.List;
 
@@ -19,10 +22,13 @@ public class MenuController {
 
     //메뉴 목록
     @GetMapping("/menus")
-    public String menuList(Model model) {
+    public String menuList(@SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember
+                           , Model model) {
+
         List<Menu> menus = menuService.findMenus();
         model.addAttribute("menus", menus);
         log.info("menu page");
+        log.info("memberID = {}", loginMember.getId());
         return "menus/menuList";
     }
 }
