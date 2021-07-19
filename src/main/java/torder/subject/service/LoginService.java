5dd530ddd28e8ac4671e46eb98830764e85d7fb5
korvata <korvata@naver.com>
2021-07-19 21:42:01 +0900

    로그인 기능

---
+++ src/main/java/torder/subject/service/LoginService.java
@@ -0,0 +1,19 @@
+package torder.subject.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import torder.subject.domain.Member;
+import torder.subject.repository.MemberRepository;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+
+    private final MemberRepository memberRepository;
+
+    public Member login(String loginId, String password) {
+        return memberRepository.findByLoginId(loginId)
+                .filter(m -> m.getPwd().equals(password))
+                .orElse(null);
+    }
+}
