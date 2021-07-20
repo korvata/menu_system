945cf85bfaa50a4d57c269fed3a007d3cfc9e628
korvata <korvata@naver.com>
2021-07-20 12:54:07 +0900

    주문 후 장바구니 삭제

--- src/main/java/torder/subject/controller/OrderController.java
+++ src/main/java/torder/subject/controller/OrderController.java
@@ -5,16 +5,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.weaver.ast.Or;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import torder.subject.domain.Cart;
-import torder.subject.domain.Menu;
-import torder.subject.domain.Order;
+import org.springframework.web.bind.annotation.*;
+import torder.subject.domain.*;
 import torder.subject.repository.MemberRepository;
 import torder.subject.repository.MenuRepository;
 import torder.subject.service.OrderService;
+import torder.subject.session.SessionConst;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,22 +26,23 @@ public class OrderController {
     //주문
     @PostMapping("/order")
     @ResponseBody
-    public void order(@RequestParam(value = "menuArr[]") List<Long> menuArr) {
+    public void order(@RequestParam(value = "menuId[]") List<Long> ids,
+                      @RequestParam(value = "count[]") List<Integer> cnts,
+                      @SessionAttribute(name = SessionConst.LOGIN_MEMBER, required = false) Member loginMember) {
 
-        String memberId = "yjh";//임시 사용자ID
+        String memberId = loginMember.getId();
 
-        for(Long menu : menuArr){
-            log.info("{}",menu);
+        for(Long id : ids){
+            log.info("menuId : {}", id);
+        }
+
+        for(Integer cnt : cnts){
+            log.info("count : {}", cnt);
         }
 
         List<Cart> carts = new ArrayList<>();
 
-        for(Long menuId : menuArr){
-            Cart cart = new Cart();
-            cart.setMenu(menuRepository.findOne(menuId));
-            cart.setCount(1);
-            carts.add(cart);
-        }
+
 
         Long orderId = orderService.order(memberId, carts);
 
