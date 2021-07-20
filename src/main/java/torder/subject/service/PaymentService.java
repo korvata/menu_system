04cd9a818d29c40ce19a9fbad66c5028b1a57191
korvata <korvata@naver.com>
2021-07-21 00:08:10 +0900

    결제상태 Y 인것만 조회되는 메서드 추가

--- src/main/java/torder/subject/service/PaymentService.java
+++ src/main/java/torder/subject/service/PaymentService.java
@@ -30,12 +30,28 @@ public class PaymentService {
 
         payment.setStatus(PaymentStatus.Y);     //결제완료
 
-        orderRepository.deleteById(orderId);    //주문내역 삭제
+        //orderRepository.deleteById(orderId);    //주문내역 삭제
     }
 
     //결제내역 조회
     public List<Payment> findPayments(){
 
         return paymentRepository.findAll();
+    }
+
+    //결제상태가 Y인 결제내역만 조회
+    public List<Payment> findYPayments(){
+
+        List<Payment> payments = paymentRepository.findAll();
+
+        List<Payment> paymented = new ArrayList<>();
+
+        for(Payment payment : payments){
+            if(payment.getStatus().equals(PaymentStatus.Y)){
+                paymented.add(payment);
+            }
+        }
+
+        return paymented;
     }
 }
