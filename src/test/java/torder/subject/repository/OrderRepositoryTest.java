d80d6cd68b9f079d348f7355ea3c30ae20cc09ca
korvata <korvata@naver.com>
2021-07-22 01:02:33 +0900

    주문 내역 조회 테스트

--- src/test/java/torder/subject/repository/OrderRepositoryTest.java
+++ src/test/java/torder/subject/repository/OrderRepositoryTest.java
@@ -1,7 +1,31 @@
 package torder.subject.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import torder.subject.domain.Order;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class OrderRepositoryTest {
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Test
+    public void testOrder(){
+        //given
+        List<Order> orderList = new ArrayList<>();
+
+        //when
+        orderList = orderRepository.findAll();
+        System.out.println(orderList);
+
+        //then
+        assertThat(orderList.size()).isEqualTo(4);   //통과되지 않음(주문내역은 총 1건)
+        assertThat(orderList.size()).isEqualTo(1);   //통과됨
+    }
 
 }
