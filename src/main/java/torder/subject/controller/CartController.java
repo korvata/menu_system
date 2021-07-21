03c4074d9cd8fe1473cb5094963886ff995d1e57
korvata <korvata@naver.com>
2021-07-21 21:11:11 +0900

    안쓰는 도메인 삭제

--- src/main/java/torder/subject/controller/CartController.java
+++ src/main/java/torder/subject/controller/CartController.java
@@ -7,7 +7,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import torder.subject.domain.Cart;
 import torder.subject.domain.Member;
-import torder.subject.domain.Menu;
 import torder.subject.service.MenuService;
 import torder.subject.session.SessionConst;
 
